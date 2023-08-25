81875457fa6bb0ae788ce4fbd3095be38bbb2772
tarun028 <tarunsrivastavakvs@gmail.com>
2023-08-25 21:09:15 +0530

    python phir se

--- Basics/Hey.py
+++ Basics/Hey.py
@@ -1,2 +1,14 @@
 print("Jai Shree Ram")
 #Variables
+a = 3
+print(a)
+b=55
+print(b)
+a = 3.14 
+print(a)
+c = True
+d= "Tarun"
+print(c)
+print(d)
+e = None
+print(e)  
---
+++ Basics/operator.py
@@ -0,0 +1,6 @@
+num1 = 10
+num2 = 2
+
+print("Sum", num1+num2)
+print("Minus", num1-num2)
+print("Mul", num1*num2)
