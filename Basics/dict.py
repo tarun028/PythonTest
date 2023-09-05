05c4935a665fec1bb305a0c88c63e50bc00233c7
tarun028 <tarunsrivastavakvs@gmail.com>
2023-09-05 22:32:27 +0530

    huhh

---
+++ Basics/dict.py
@@ -0,0 +1,14 @@
+dict1 = {}
+print(type(dict1))
+dict2 = set()
+print(type(dict2))
+
+
+a = {"hey":"bro", "hi":"miss", "yo":"bro"}
+b = {"t":1,"a":2}
+
+print(a["hi"])
+print(b["t"])
+
+b["r"]=3
+print(b)
