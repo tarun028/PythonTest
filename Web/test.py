92084c9d81ea8a157a6c5443a27e8fa94f642435
tarun028 <tarunsrivastavakvs@gmail.com>
2023-09-10 22:19:30 +0530

    data scrapping lol

---
+++ Web/test.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("https://www.geeksforgeeks.org/")
+
+soup = BeautifulSoup(req.content, "html.parser")
+
+
+#print(soup.prettify())
+print(soup.get_text())
